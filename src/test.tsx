691f3d23362cb8285663a680694a0e277f41fc19
Vladimir Bezhenar <me@vbezhenar.com>
2023-06-29 18:38:58 +0600

    Initial commit

---
+++ src/test.tsx
@@ -0,0 +1,15 @@
+/** @jsx myJsxFactory */
+
+export function MyReport(): MyElement {
+  return <MyPage />;
+}
+
+function MyPage(): MyElement {
+  return new MyElement();
+}
+
+function myJsxFactory(): MyElement {
+  return new MyElement();
+}
+
+class MyElement {}
